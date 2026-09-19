54b876b49ed50412f2f7f9d5b5e43dc68b9373d8
Swathi <sm7274272@gmail.com>
2026-09-19 10:50:24 +0530

    Added User Class

---
+++ User.java
@@ -0,0 +1,12 @@
+public class User {
+
+    String name;
+    String email;
+    String password;
+
+    User(String name, String email, String password) {
+        this.name = name;
+        this.email = email;
+        this.password = password;
+    }
+}
