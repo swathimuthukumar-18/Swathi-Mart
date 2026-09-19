cf6eb1c4a3873775603a724eb8d67eed764874bc
Swathi <sm7274272@gmail.com>
2026-09-19 10:58:46 +0530

    Added Payment class

---
+++ Payment.java
@@ -0,0 +1,14 @@
+
+public class Payment {
+
+    int amount;
+
+    Payment(int amount) {
+        this.amount = amount;
+    }
+
+    void makePayment() {
+        System.out.println("Payment successful!");
+        System.out.println("Amount Paid: Rs." + amount);
+    }
+}
